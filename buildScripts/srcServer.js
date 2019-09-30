a5a1aea2dd27fd189fe17fd8f901a3249d7b74d8
chikabee <will.lawson@yahoo.com>
2019-09-30 19:01:35 -0400

    Babel configurations and edits

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+// var express = require('express');
+// var path = require('path');
+// var open = require('open');
 
-var port = 3000;
-var app = express();
+// var port = 3000;
+// var app = express();
+
+import express from 'express';
+import path from 'path';
+import open from 'open';
+
+const port = 3000;
+const app = express();
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../src/index.html'));
--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,7 @@
-var chalk = require('chalk');
+// CommonJS
+//var chalk = require('chalk');
+// or
+// ES6  module syntax
+import chalk from 'chalk';
 
 console.log(chalk.green('starting app in dev mode...'));
