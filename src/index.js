e9d4010ee2ebc4514c6fb0c0b4a60fff29f7f55d
chikabee <will.lawson@yahoo.com>
2019-10-01 12:53:02 -0400

    use a sourcemap to support debugging

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import './index.css';
 import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
+
+debugger;
 // these are back-ticks (ES6 template string)
 console.log(`I would pay ${courseValue} for this awesome course!`);
