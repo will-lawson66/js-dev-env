5573322018aaa3c22cd68829e6d2ecea6e777adc
chikabee <will.lawson@yahoo.com>
2019-10-02 09:54:05 -0400

    eslint overrides, clean all linting issues

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -4,4 +4,4 @@
 // ES6  module syntax
 import chalk from 'chalk';
 
-console.log(chalk.green('starting app in dev mode...'));
+console.log(chalk.green('starting app in dev mode...')); //eslint-disable-line no-console
--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
 
-debugger;
+//debugger;
 // these are back-ticks (ES6 template string)
-console.log(`I would pay ${courseValue} for this awesome course!`);
+console.log(`I would pay ${courseValue} for this awesome course!`); //eslint-disable-line no-console
