e51d49b43642980c3e2924eb82d3b1b76f299e8e
chikabee <will.lawson@yahoo.com>
2019-10-03 16:00:50 -0400

    set up api to perform GET of user data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import './index.css';
 
+import {getUsers, deleteUser} from './api/userApi';
+
+// Populate table of users via API call.
+getUsers().then(result => {
+  let usersBody = "";
+
+  result.forEach(user => {
+    usersBody+= `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+});
+
+
 import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
@@ -7,3 +27,5 @@ const courseValue = numeral(1000).format('$0,0.00');
 //debugger;
 // these are back-ticks (ES6 template string)
 console.log(`I would pay ${courseValue} for this awesome course!`); //eslint-disable-line no-console
+
+
